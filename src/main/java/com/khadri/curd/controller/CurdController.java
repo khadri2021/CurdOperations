633a1fe7ce05f61f4713c3b7a70b3e372407e3fe
KHADRI <khadri.institutions@gmail.com>
2022-05-03 12:59:52 +0530

    updated code

--- src/main/java/com/khadri/curd/controller/CurdController.java
+++ src/main/java/com/khadri/curd/controller/CurdController.java
@@ -1,9 +1,15 @@
 package com.khadri.curd.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +23,56 @@ public class CurdController {
 	CurdDao curdDao;
 	
 	
-	@RequestMapping(value = "studentinfo",method = RequestMethod.GET)
+	@GetMapping(value = "studentinfo")
 	public ResponseEntity<Student> getStudentDetails(@RequestParam  String UUID) {
 		
 		Student studentInfo = curdDao.getStudent(UUID);	
 		
 		return ResponseEntity.ok(studentInfo);
+	}
+	
+	
+	@PostMapping(value = "studentinfo")
+	public ResponseEntity<String> postStudentDetails(@RequestBody Student student){
+		
+		String UUID = curdDao.insertStudent(student);
+		
+		ResponseEntity<String> entity= new ResponseEntity<>(UUID, HttpStatus.CREATED);
+		
+		return entity;
+	}
+	
+	
+	@PutMapping(value = "studentinfo")
+	public ResponseEntity<String> putStudentDetails(@RequestBody Student student){
+		
+		  curdDao.updateOrReplaceStudent(student);
+		
+		ResponseEntity<String> entity= new ResponseEntity<>("Updated/Replace Successfuly!!!",HttpStatus.OK);
+		
+		return entity;
+	}
+	
+	
+	@PatchMapping(value = "studentinfo")
+	public ResponseEntity<String> patchStudentDetails(@RequestBody Student student){
+		
+		  curdDao.updateStudent(student);
+		
+		ResponseEntity<String> entity= new ResponseEntity<>("Updated Successfuly!!!",HttpStatus.OK);
+		
+		return entity;
+	}
+	
+	
+	@DeleteMapping(value = "studentinfo/{UUID}")
+	public ResponseEntity<String> deleteStudentDetails(@PathVariable String UUID){
+		
+		  curdDao.deleteStudent(UUID);
+		
+		ResponseEntity<String> entity= new ResponseEntity<>("Deleted Successfuly!!!",HttpStatus.NO_CONTENT);
+		
+		return entity;
 	}
 	
 }
--- src/main/java/com/khadri/curd/dao/CurdDao.java
+++ src/main/java/com/khadri/curd/dao/CurdDao.java
@@ -1,7 +1,10 @@
 package com.khadri.curd.dao;
 
+import java.util.Objects;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -24,5 +27,57 @@ public class CurdDao {
 		
 		return std;
 		
+	}
+	
+	
+	public String insertStudent(Student student) {
+		 
+		  Session session = sessionFactory.openSession();
+		  Transaction txn = session.beginTransaction();
+		  	
+		 String uuid = (String) session.save(student);
+		  
+		  txn.commit();
+	 
+		return uuid;
+		
+	}
+	
+	public void updateOrReplaceStudent(Student student) {
+		 
+		  Session session = sessionFactory.openSession();
+		  Transaction txn = session.beginTransaction();
+		  	
+		  session.merge(student);
+		  
+		  txn.commit();
+	 
+	}
+	
+	
+	public void updateStudent(Student student) {
+		 
+		  Session session = sessionFactory.openSession();
+		  Transaction txn = session.beginTransaction();
+		  	
+		  session.update(student);
+		  
+		  txn.commit();
+	 
+	}
+	
+	
+	public void deleteStudent(String uuid) {
+		 
+		  Session session = sessionFactory.openSession();
+		  Transaction txn = session.beginTransaction();
+		  	
+		  Student student = session.get(Student.class, uuid);
+		  
+		  if(Objects.nonNull(student))
+			  	session.delete(student);
+		  
+		  txn.commit();
+	 
 	}
 }
