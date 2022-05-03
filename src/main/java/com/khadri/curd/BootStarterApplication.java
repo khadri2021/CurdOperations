ecebe93015d1588decbb5b8769848a59bbd88bad
KHADRI SOFTWARE INSTITUTIONS <86885935+khadri2021@users.noreply.github.com>
2022-05-03 10:07:06 +0530

    Add files via upload
    initial changes setup

---
+++ src/main/java/com/khadri/curd/BootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.khadri.curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootStarterApplication.class, args);
+	}
+
+}
